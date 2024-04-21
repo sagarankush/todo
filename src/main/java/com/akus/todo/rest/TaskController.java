ba177b6b95b3c6e379b24ad9f109621a2dd948c6
Ankush <ankushsagar30@gmail.com>
2024-04-21 23:48:16 +0530

    basic rest api for user and tasks

--- src/main/java/com/akus/todo/rest/TaskController.java
+++ src/main/java/com/akus/todo/rest/TaskController.java
@@ -2,18 +2,25 @@ package com.akus.todo.rest;
 
 import com.akus.todo.data.TaskRepository;
 import com.akus.todo.model.Task;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/rest", produces = "application/json")
 public class TaskController {
     TaskRepository taskRepository;
 
     public TaskController(TaskRepository taskRepository){
         this.taskRepository = taskRepository;
     }
-    @GetMapping("/rest/getall")
+    @GetMapping(path = "/getall")
     public Iterable<Task> getAllTasks(){
         return taskRepository.findAll();
+    }
+
+    @PostMapping(path = "/savetask", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task saveTask(@RequestBody Task task){
+        return taskRepository.save(task);
     }
 }
--- src/main/java/com/akus/todo/rest/UsersController.java
+++ src/main/java/com/akus/todo/rest/UsersController.java
@@ -3,16 +3,18 @@ package com.akus.todo.rest;
 import com.akus.todo.data.UsersRepository;
 import com.akus.todo.model.Users;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/rest", produces = "application/json")
 public class UsersController {
     UsersRepository usersRepository;
 
     public UsersController(UsersRepository usersRepository){
         this.usersRepository = usersRepository;
     }
-    @GetMapping("/rest/getalluser")
+    @GetMapping("/getalluser")
     public Iterable<Users> getAllTasks(){
         return usersRepository.findAll();
     }
